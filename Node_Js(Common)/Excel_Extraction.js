692ab2e24f72a7a30d55f0025bf67d4391fddcd8
kimjongjae <54018433+kimjongjae@users.noreply.github.com>
2024-06-19 00:46:17 +0900

    서버 커밋

--- Node_Js(Common)/Excel_Extraction.js
+++ Node_Js(Common)/Excel_Extraction.js
@@ -15,9 +15,9 @@ function excelToJson(excelFilePath) {
 }
 
 // 테이블 경로
-const folderPath = './Node_Js(Common)/JsonData'; 
+const folderPath = '../TableData'; 
 // Json폴더 경로
-const Json_folderPath = './Node_Js(Common)/JsonData'; 
+const Json_folderPath = './JsonData'; 
 
 // 폴더 내 파일 목록 읽기
 fs.readdir(folderPath, (err, files) => {
@@ -31,13 +31,15 @@ fs.readdir(folderPath, (err, files) => {
         // 파일의 절대 경로 생성
         const filePath = path.join(folderPath, file);
 
+
         // 파일 확장자가 .xlsx 인 경우에만 처리
         if (path.extname(file).toLowerCase() === '.xlsx') {
+
             // 엑셀 파일을 JSON으로 변환
             const jsonData = excelToJson(filePath);
 
             // JSON 데이터를 파일로 저장 (예시: 파일명.xlsx -> 파일명.json)
-            const jsonFileName = path.basename(file.sheetName, '.xlsx') + '.json';
+            const jsonFileName = path.basename(file, '.xlsx') + '.json';
             const jsonFilePath = path.join(Json_folderPath, jsonFileName);
             fs.writeFileSync(jsonFilePath, JSON.stringify(jsonData, null, 2));
 
